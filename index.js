0ae0c6c7d416dd60c801f4e404e7b1a8e96f824d
NoorAthamny <athamnynoor@gmail.com>
2024-09-29 16:42:25 +0300

    Card class - showing the properties of the card

---
+++ index.js
@@ -0,0 +1,9 @@
+// Creating Card class that holds the suits the values
+// that shoes the card properties
+class Card {
+  constructor(rank, suit, value) {
+    this.rank = rank;
+    this.value = value;
+    this.suit = suit;
+  }
+}
