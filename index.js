3550af51973e1ec703994d2faee7441fc375d167
NoorAthamny <athamnynoor@gmail.com>
2024-09-29 16:48:49 +0300

    Deck class with cards the values

--- index.js
+++ index.js
@@ -7,3 +7,31 @@ class Card {
     this.suit = suit;
   }
 }
+
+// creating deck of cards
+
+class Deck {
+  constructor() {
+    this.suits = ["hearts", "diamonds", "clubs", "spades"];
+    this.cards = this.createDeck();
+    this.shuffleDeck();
+  }
+
+  createDeck() {
+    const cardRanks = [
+      { rank: "Ace", value: 1 },
+      { rank: "2", value: 2 },
+      { rank: "3", value: 3 },
+      { rank: "4", value: 4 },
+      { rank: "5", value: 5 },
+      { rank: "6", value: 6 },
+      { rank: "7", value: 0 },
+      { rank: "8", value: 8 },
+      { rank: "9", value: 9 },
+      { rank: "10", value: 10 },
+      { rank: "Jack", value: -1 },
+      { rank: "Queen", value: 12 },
+      { rank: "King", value: 13 },
+    ];
+  }
+}
